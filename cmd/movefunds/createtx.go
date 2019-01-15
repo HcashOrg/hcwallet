e6d2734cb788bbab08929627d9ec1212753c3680
TriRikkie <13641537547@163.com>
2019-01-15 17:28:06 +0800

    deal

--- cmd/movefunds/createtx.go
+++ cmd/movefunds/createtx.go
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2016-2017 The Hc developers
- *
+ * Copyright (c) 2016-2017 The Decred developers
+ * Copyright (c) 2018-2020 The Hc developers
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
--- pgpwordlist/pgpwordlist.go
+++ pgpwordlist/pgpwordlist.go
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2015-2016 The Hc developers
+ * Copyright (c) 2015 The Decred developers
+ * Copyright (c) 2018-2020 The Hc developers
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
