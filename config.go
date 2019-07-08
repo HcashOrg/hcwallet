eef785a073fdfabee1b52f4365ee94adbb02db96
robinsonking <878950187@qq.com>
2019-07-08 17:19:08 +0800

    commit modification of hcwallet.

--- config.go
+++ config.go
@@ -36,7 +36,7 @@ const (
 	defaultRPCMaxClients       = 10
 	defaultRPCMaxWebsockets    = 25
 	defaultEnableTicketBuyer   = false
-	defaultEnableOmni          = false
+	defaultEnableOmni          = true
 	defaultEnableVoting        = false
 	defaultReuseAddresses      = false
 	defaultRollbackTest        = false
