202c0a1583479ccf100f9cd64fd3a1a2f4888104
robinsonking <878950187@qq.com>
2019-07-19 17:39:57 +0800

    commit modification of hcwallet.

--- aiticketbuyer/buyer.go
+++ aiticketbuyer/buyer.go
@@ -826,7 +826,7 @@ func (t *TicketPurchaser) Purchase(height int64) (*PurchaseStats, error) {
 
 	// Ticket purchase requires 2 blocks to confirm
 	expiry := int32(int(height) + t.ExpiryDelta() + 2)
-	hashes, purchaseErr := t.wallet.PurchaseTickets(0,
+	hashes, purchaseErr := t.wallet.PurchaseAITickets(0,
 		maxPriceAmt,
 		0, // 0 minconf is used so tickets can be bought from split outputs
 		ticketAddress,
